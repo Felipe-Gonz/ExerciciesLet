029d204cd215daddebc3a02b460dee0644e2b0f6
Felipe-Gonz <fg180899@gmail.com>
2025-06-29 23:14:22 -0600

    Palindrome finish

--- src/main/java/org/example/easy/Palindrome.java
+++ src/main/java/org/example/easy/Palindrome.java
@@ -2,7 +2,27 @@ package org.example.easy;
 
 public class Palindrome {
 
-    int x;
-    int reverse;
+    public int x;
+    int digit;
+
+    public boolean evaluar(){
+        int reversed = 0;
+        int original = x;
+        int n = x;
+
+        if(x < 0){
+            return false;
+        }if( x == 0){
+            return true;
+        }
+
+        while(n!= 0) {
+            digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n = n / 10;
+        }
+        return (original == reversed);
+
+    }
 
 }
