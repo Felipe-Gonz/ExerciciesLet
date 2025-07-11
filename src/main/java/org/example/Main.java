954be74c35164013ed0351a016bdb67344a54272
Felipe-Gonz <fg180899@gmail.com>
2025-07-10 23:14:59 -0600

    Commit TowList

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,6 @@
 package org.example;
 
-import org.example.easy.CommonPrefix;
-import org.example.easy.Palindrome;
-import org.example.easy.Parentheses;
-import org.example.easy.RomanToInteger;
+import org.example.easy.*;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -24,9 +21,26 @@ public class Main {
         String[] ejemplo = {"flower", "flow", "flight"};
         System.out.println(commonPrefix.Prefijo(ejemplo)); **/
 
-        Parentheses parentheses = new Parentheses();
+        /*Parentheses parentheses = new Parentheses();
         Boolean parentesis = parentheses.isValid("(()");
-        System.out.println("Resultado:" + parentesis);
+        System.out.println("Resultado:" + parentesis);**/
+
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        TowList towList = new TowList();
+        ListNode merged = towList.mergeTwoLists(list1, list2);
+
+        while (merged != null){
+            System.out.println(merged.val + " ");
+            merged = merged.next;
+        }
+
 
     }
 }
---
+++ src/main/java/org/example/easy/ListNode.java
@@ -0,0 +1,17 @@
+package org.example.easy;
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    ListNode() {}
+
+    public ListNode(int val){
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next){
+        this.val = val;
+        this.next = next;
+    }
+}
